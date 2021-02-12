cea144b6a0be621eafe982a69046cd6727454264
Craig Bernal <Craig.Bernal@outlook.com>
2021-02-11 20:37:54 -0500

    Box of Rocks!!!

--- SpaceGame/Ship.cs
+++ SpaceGame/Ship.cs
@@ -3,17 +3,30 @@
     // EXAMPLE SHIP FOR DEMO ONLY - Jeffrey
     class Ship
     {
-        public int HullIntegrity { get; set; }
-        public int Coins { get; set; }
-        public int Fuel { get; set; }
         public int Time { get; set; }
-
+        public int Coins { get; set; }
+        public int DarkMatter { get; set; }
+        public int DarkMatterMax { get; set; }  //Tuple?
+        public int Gold { get; set; }
+        public int GoldMax { get; set; }       //Tuple?
+        public int Hull { get; set; }
+        public int HullMax { get; set; }       //Tuple?
+        public string Upgrade { get; set; }
+        public int Fuel { get; set; }
+        public int FuelCapacity { get; set; }
+        public int HullIntegrity { get; set; }
+        public int ResourceCapacity { get; set; }
         public Ship()
         {
-            Coins = 100;
-            HullIntegrity = 100;
-            Fuel = 100;
             Time = 500;
+            Coins = 0;
+            DarkMatter = 0;
+            DarkMatterMax = 5000;
+            Gold = 0;
+            GoldMax = 5000;
+            Hull = 0;
+            HullMax = 5000;
+            Upgrade = " ";
         }
 
         public override string ToString()
